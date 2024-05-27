189722a2feac2ee3434de72bbdd63e612cb91641
modabbir116 <modabber.web@gmail.com>
2024-05-27 12:11:09 +0600

    js practise array

--- array.js
+++ array.js
@@ -18,17 +18,40 @@ const myName = ["Modabbir", "Masum", "Nuruddin", "Mamun", "Jahid", "Ratul"];
 for(item of myName){
     console.log(item);
 }
-console.log(" for loops ==================");
+console.log("=========== for loops ==================");
 // for loops 
 for(let item = 0; item<myName.length; item++){
     console.log(myName[item]);
 }
-console.log("while loops ==================");
+console.log("============ while loops ==================");
 // while loops 
 let items = 0;
 while(items<myName.length){
     console.log(myName[items]);
     items++
 }
-
+console.log("=======Do While loops  ==================");
 // Do While loops 
+let itemss = 0;
+do{
+    console.log(myName[itemss]);
+    itemss++
+}while(itemss<myName.length);
+
+console.log("=======for in loops  ==================");
+// for in loops 
+for(item11 in myName){
+    console.log(myName[item11]);
+}
+
+console.log("======= forEach Function  ==================");
+// JavaScript Array forEach()
+myName.forEach((curentValu, index, arr) =>{
+    console.log(`${curentValu} ${index}`);
+})
+
+console.log("======= Map Function  ==================");
+// JavaScript Array map()
+myName.map((names, index) =>{
+    console.log(names, index);
+})
